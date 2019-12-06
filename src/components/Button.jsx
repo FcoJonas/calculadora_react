223e2e4ddf22ecab47e1d6ad33215d69c0ddf2b4
jonas <jonas@itarget.com.br>
2019-12-06 16:22:16 -0300

    Buttons e calculator jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,11 @@ import React, { Component } from 'react'
 import './Button.css'
 //import Button from './Button'
 
-export default props => <button className={`
-button
-${props.operation ? 'operation' : ''}
-
-`}>{props.label}</button>
+export default props =>
+    <button onClick={e => props.click && props.click(props.label)} className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+        `}>
+        {props.label}</button>
